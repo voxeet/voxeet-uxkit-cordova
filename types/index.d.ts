6a580e335478858cccc97c92868478ead9613f24
codlab <codlabtech@gmail.com>
2018-05-18 16:36:42 +0200

    initial version

---
+++ types/index.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for Cordova Voxeet plugin
+// Project: https://github.com/voxeet/cordova-plugin-voxeet
+// Definitions by: Microsoft Open Technologies Inc <http://msopentech.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// 
+// Copyright (c) Microsoft Open Technologies Inc
+// Licensed under the MIT license. 
+
+interface Voxeet {
+
+    initialize(consumerKey: string, consumerSecret: string): Promise;
+
+    openSession(userId: string, participantName: string, avatarUrl: string): Promise;
+
+    closeSession(userId: string, participantName: string, avatarUrl: string): Promise;
+
+    startConference(conferenceId: string, participants: UserInfo[]): Promise;
+
+    stopConference(): Promise;
+
+    add(/* participant */): Promise;
+
+    update(/* participant */): Promise;
+
+    remove(/* participant */): Promise;
+
+    appearMaximized(enabled: bool): Promise;
+
+    defaultBuiltInSpeaker(enabled: bool): Promise;
+
+    screenAutoLock(enabled: bool): Promise;
+}
+
+interface UserInfo {
+    json(): Object;
+}
---
+++ www/Voxeet.js
@@ -0,0 +1,84 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * 'License'); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+/**
+ * This class enable interaction with Voxeet ConferenceKit
+ * @constructor
+ */
+const exec = require('cordova/exec');
+const SERVICE = 'Voxeet';
+
+class Voxeet {
+
+    initialize (consumerKey, consumerSecret) {
+        return new Promise((resolve, reject) => {
+            exec(resolve, reject, SERVICE, 'initialize', [consumerKey, consumerSecret]);
+        });
+    }
+
+    openSession (userId, participantName, avatarUrl) {
+        return new Promise((resolve, reject) => {
+            exec(resolve, reject, SERVICE, 'openSession', [userId, participantName, avatarUrl]);
+        });
+    }
+
+    closeSession (userId, participantName, avatarUrl) {
+        return new Promise((resolve, reject) => exec(resolve, reject, SERVICE, 'closeSession', []));
+    }
+
+    startConference (conferenceId, participants) {
+        const array = participants ? participants.map(e => e.json()) : null;
+        return new Promise((resolve, reject) => {
+            exec(resolve, reject, SERVICE, 'startConference', [conferenceId, array]);
+        });
+    }
+
+    stopConference () {
+        return new Promise((resolve, reject) => exec(resolve, reject, SERVICE, 'stopConference', []));
+    }
+
+    add (/* participant */) {
+        exec(null, null, SERVICE, 'add', [null]);
+    }
+
+    update (/* participant */) {
+        exec(null, null, SERVICE, 'update', [null]);
+    }
+
+    remove (/* participant */) {
+        exec(null, null, SERVICE, 'remove', [null]);
+    }
+
+    appearMaximized (enabled) {
+        exec(null, null, SERVICE, 'appearMaximized', [enabled]);
+    }
+
+    defaultBuiltInSpeaker (enabled) {
+        exec(null, null, SERVICE, 'defaultBuiltInSpeaker', [enabled]);
+    }
+
+    screenAutoLock (enabled) {
+        exec(null, null, SERVICE, 'screenAutoLock', [enabled]);
+    }
+
+}
+
+module.exports = new Voxeet(); // will be available through Voxeet not voxeet -> fake 'singleton'
