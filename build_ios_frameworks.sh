88fa695775d0831bdc5015fec099d4c266de897b
codlab <codlabtech@gmail.com>
2018-12-18 17:52:29 +0100

    skip iOS build on install

--- build_ios_frameworks.sh
+++ build_ios_frameworks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+[[ "${VOXEET_SKIP_IOS_BUILD}" == "true" ]] && echo "skip build ios" && exit 0
+
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 echo "starting carthage in $CURR_DIR"
