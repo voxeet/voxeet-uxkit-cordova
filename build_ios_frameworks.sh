095a7296c007d8ee553d1f75967321c045108b14
Corentin Larroque <co.larroque@gmail.com>
2019-03-07 11:33:48 +0100

    Patch production installation dependencies.

--- build_ios_frameworks.sh
+++ build_ios_frameworks.sh
@@ -2,5 +2,5 @@
 
 [[ "${VOXEET_SKIP_IOS_BUILD}" == "true" ]] && echo "skip build ios" && exit 0
 
-echo "starting carthage"
-carthage update --platform ios --verbose --project-directory `dirname "$0"`/src/ios
+echo "Installing \"cordova-plugin-voxeet\" ios dependencies (carthage)"
+carthage update --platform ios --project-directory `dirname "$0"`/src/ios
