8e7da8a8c341c585a939b869b85dafac480e2477
codlab <codlabtech@gmail.com>
2019-07-24 16:31:21 +0200

    set new version 1.2.2

--- www/VoxeetSDK.d.ts
+++ www/VoxeetSDK.d.ts
@@ -45,7 +45,7 @@ declare class Voxeet_ {
     refreshToken: TokenRefreshCallback | undefined;
     constructor();
     initialize(consumerKey: string, consumerSecret: string): Promise<any>;
-    initializeToken(accessToken: string | undefined, refreshToken: TokenRefreshCallback): Promise<unknown>;
+    initializeToken(accessToken: string | undefined, refreshToken: TokenRefreshCallback): Promise<{}>;
     connect(userInfo: UserInfo): Promise<any>;
     disconnect(): Promise<any>;
     create(options: CreateOptions): Promise<any>;
@@ -61,8 +61,8 @@ declare class Voxeet_ {
     appearMaximized(enabled: boolean): Promise<any>;
     defaultBuiltInSpeaker(enabled: boolean): Promise<any>;
     defaultVideo(enabled: boolean): Promise<any>;
-    startRecording(): Promise<unknown>;
-    stopRecording(): Promise<unknown>;
+    startRecording(): Promise<{}>;
+    stopRecording(): Promise<{}>;
     screenAutoLock(enabled: boolean): Promise<any>;
     isUserLoggedIn(): Promise<boolean>;
     checkForAwaitingConference(): Promise<any>;
@@ -70,8 +70,8 @@ declare class Voxeet_ {
     stopConference(): Promise<any>;
     openSession(userInfo: UserInfo): Promise<any>;
     closeSession(): Promise<any>;
-    onAccessTokenOk(accessToken: string): Promise<unknown>;
-    onAccessTokenKo(errorMessage: string): Promise<unknown>;
+    onAccessTokenOk(accessToken: string): Promise<{}>;
+    onAccessTokenKo(errorMessage: string): Promise<{}>;
 }
 export declare const Voxeet: Voxeet_;
 export {};
