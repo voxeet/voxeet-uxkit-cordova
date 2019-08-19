15d9de7ef21f74704f77d493e99513ffccc036b2
codlab <codlabtech@gmail.com>
2019-08-19 17:45:02 +0200

    update install script for iOS to make it more resilient

--- build_ios_frameworks.js
+++ build_ios_frameworks.js
@@ -1,9 +1,23 @@
-#!/usr/bin/env node
+module.exports = function(context) {
+  var fs = context.requireCordovaModule('fs');
+  var Q = context.requireCordovaModule('q');
+  var exec = context.requireCordovaModule('child_process').exec;
+  var deferral = new Q.defer();
 
-var exec = require('child_process').exec;
+  const dir = context.opts.plugin.dir;
+  const skip = "true" === process.env.VOXEET_SKIP_IOS_BUILD;
 
-function exec_callback(error, stdout, stderr) {
-  console.log(stdout);
+  function exec_callback(error, stdout, stderr) {
+    error && console.log(error);
+    stdout && console.log(stdout);
+    stderr && console.log(stderr);
+  }
+  
+  if (!skip && fs.existsSync(dir)) {
+    exec(`pushd ${dir} ; sh ./build_ios_frameworks.sh ; popd`, exec_callback);
+  } else {
+    console.log("skipping script installation...");
+  }
+
+  return deferral.promise;
 }
-
-exec("sh ./build_ios_frameworks.sh", exec_callback);
--- build_ios_frameworks.sh
+++ build_ios_frameworks.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "use of this script is deprecated and will be removed in the future"
-
-[[ "${VOXEET_SKIP_IOS_BUILD}" == "true" ]] && echo "skip build ios" && exit 0
+echo "use of this script is deprecated and will be removed in the future -- if using from the plugin add command, nothing to do"
 
 echo "Installing \"cordova-plugin-voxeet\" ios dependencies (carthage)"
 carthage update --platform ios --project-directory `dirname "$0"`/src/ios
