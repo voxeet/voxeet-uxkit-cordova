cc7641508bc08de2c3e5d08b62464f0a2d562ca4
codlab <codlabtech@gmail.com>
2019-10-28 22:12:57 +0100

    fix typo making returning the class name instead of full name

--- src/android/com/voxeet/toolkit/notification/CordovaIncomingNotification.java
+++ src/android/com/voxeet/toolkit/notification/CordovaIncomingNotification.java
@@ -2,9 +2,7 @@ package com.voxeet.toolkit.notification;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
-import com.voxeet.sdk.utils.AndroidManifest;
 import com.voxeet.toolkit.incoming.IncomingNotification;
 
 public class CordovaIncomingNotification extends IncomingNotification {
@@ -16,7 +14,7 @@ public class CordovaIncomingNotification extends IncomingNotification {
     @Override
     public String getIncomingAcceptedClass(@NonNull Context context) {
         //not returning super since this activity will be responsible for that
-        return CordovaIncomingFromNotificationActivity.class.getSimpleName();
+        return CordovaIncomingFromNotificationActivity.class.getName(); //canonical would have returned name
     }
 
 
