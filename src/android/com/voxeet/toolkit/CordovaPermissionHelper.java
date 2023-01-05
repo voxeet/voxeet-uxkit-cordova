1eef3a284846b92cb8f2cd6e8553d23fbacb8847
Kevin Le Perf <kevin.leperf@dolby.com>
2023-01-05 05:52:48 +0100

    Update permissions asked when joining

--- src/android/com/voxeet/toolkit/CordovaPermissionHelper.java
+++ src/android/com/voxeet/toolkit/CordovaPermissionHelper.java
@@ -7,6 +7,7 @@ import com.voxeet.promise.Promise;
 import com.voxeet.uxkit.common.permissions.PermissionController;
 import com.voxeet.uxkit.common.permissions.PermissionResult;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,13 +18,18 @@ public class CordovaPermissionHelper {
 
     public static Promise<Boolean> requestDefaultPermission() {
         return new Promise<>(solver -> {
+            List<String> permissions = new ArrayList<>(PERMISSIONS);
 
             // Cordova itself is not yet bumping to tools 33 but we can use the version directly and the permission
-            if (Build.VERSION.SDK_INT >= 33 && !PERMISSIONS.contains(POST_NOTIFICATIONS)) {
-                PERMISSIONS.add(POST_NOTIFICATIONS);
+            if (Build.VERSION.SDK_INT >= 33) {
+                permissions.add(POST_NOTIFICATIONS);
             }
 
-            PermissionController.requestPermissions(PERMISSIONS).then(permissionResults -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                permissions.add(Manifest.permission.BLUETOOTH_CONNECT);
+            }
+
+            PermissionController.requestPermissions(permissions).then(permissionResults -> {
                 for (PermissionResult res : permissionResults) {
                     if (res.isFor(Manifest.permission.RECORD_AUDIO) && !res.isGranted) {
                         solver.resolve(false);
