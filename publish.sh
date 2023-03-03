0edc41f31bdb86f5d3413774572ab95a9d609729
Yuriy Ganushevich <Yuriy.Ganushevich@dolby.com>
2023-03-03 13:31:32 +0100

    Use the latest SDK 3.8.0 and component version registration

--- publish.sh
+++ publish.sh
@@ -6,11 +6,13 @@ echo Setting version in code...
 VERSION=$(node -e "console.log(require('./package.json').version);")
 echo Current version = $VERSION
 sed -i '' "s/___CORDOVA_VERSION___/$VERSION/g" src/android/com/voxeet/toolkit/VoxeetCordova.java
+sed -i '' "s/___CORDOVA_VERSION___/$VERSION/g" src/ios/CDVVoxeet.m
 
 echo building and publishing...
 npm publish || exit 1
 
 echo Reverting modification in VoxeetCordova.java...
 git checkout src/android/com/voxeet/toolkit/VoxeetCordova.java
+git checkout src/ios/CDVVoxeet.m
 
 echo done
